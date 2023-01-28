d0bee1cacb2b738274f538007e811fd48cefdd25
Daniyar <ensw01119@student.su.edu.krd>
2023-01-28 19:20:42 +0300

    feat: player section added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,43 +1,83 @@
 import { useState } from "react";
 import Square from "./Square";
 export default function Board() {
+  const [players, setPlayers] = useState(["X", "O"]);
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const [turn, setTurn] = useState("X");
+  const [turn, setTurn] = useState(players[0]);
 
   function onSquareClick(i) {
     if (calculateWinner(squares) || squares[i]) return;
     const nextSquares = squares.slice();
     nextSquares[i] = turn;
     setSquares(nextSquares);
-    turn === "X" ? setTurn("O") : setTurn("X");
+    turn === players[0] ? setTurn(players[1]) : setTurn(players[0]);
   }
   const winner = calculateWinner(squares);
   let status;
   if (winner) {
     status = "Winner: " + winner;
   } else {
-    status = 'Player "' + (turn === "X" ? "X" : "O") + '" Turn :';
+    status = (turn === players[0] ? "Player 1's" : "Player 2's") + '" Turn :';
   }
   return (
-    <div className="board">
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => onSquareClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => onSquareClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => onSquareClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => onSquareClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => onSquareClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => onSquareClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => onSquareClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => onSquareClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => onSquareClick(8)} />
-      </div>
+    <>
+      <Players players={players} onSetPlayers={setPlayers} onTurn={setTurn} />
+      <div className="board">
+        <div className="board-row">
+          <Square
+            value={squares[0]}
+            players={players}
+            onSquareClick={() => onSquareClick(0)}
+          />
+          <Square
+            value={squares[1]}
+            players={players}
+            onSquareClick={() => onSquareClick(1)}
+          />
+          <Square
+            value={squares[2]}
+            players={players}
+            onSquareClick={() => onSquareClick(2)}
+          />
+        </div>
+        <div className="board-row">
+          <Square
+            value={squares[3]}
+            players={players}
+            onSquareClick={() => onSquareClick(3)}
+          />
+          <Square
+            value={squares[4]}
+            players={players}
+            onSquareClick={() => onSquareClick(4)}
+          />
+          <Square
+            value={squares[5]}
+            players={players}
+            onSquareClick={() => onSquareClick(5)}
+          />
+        </div>
+        <div className="board-row">
+          <Square
+            value={squares[6]}
+            players={players}
+            onSquareClick={() => onSquareClick(6)}
+          />
+          <Square
+            value={squares[7]}
+            players={players}
+            onSquareClick={() => onSquareClick(7)}
+          />
+          <Square
+            value={squares[8]}
+            players={players}
+            onSquareClick={() => onSquareClick(8)}
+          />
+        </div>
 
-      <h1>{status}</h1>
-    </div>
+        <h1>{status}</h1>
+      </div>
+    </>
   );
 }
 
@@ -59,4 +99,34 @@ function calculateWinner(squares) {
     }
   }
   return null;
+}
+
+function Players({ players, onSetPlayers, onTurn }) {
+  return (
+    <form className="players">
+      <div>
+        <label className="player1" htmlFor="player1">
+          Player 1 :
+        </label>
+        <input
+          id="player1"
+          name="player1"
+          className="player1"
+          onChange={(e) => {
+            onSetPlayers([e.target.value, players[1]]);
+            onTurn(e.target.value);
+          }}
+        />
+        <label className="player2" htmlFor="player2">
+          Player 2 :{" "}
+        </label>
+        <input
+          name="player2"
+          className="player2"
+          onChange={(e) => onSetPlayers([players[0], e.target.value])}
+        />
+      </div>
+      <input className="startBtn" type="button" value="Start" />
+    </form>
+  );
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,6 +1,22 @@
-export default function Square({ value, onSquareClick }) {
+export default function Square({ value, onSquareClick, players }) {
   return (
-    <button onClick={onSquareClick} className="square">
+    <button
+      style={
+        value === players[0]
+          ? {
+              backgroundColor: "green",
+              color: "white",
+            }
+          : value === players[1]
+          ? {
+              backgroundColor: "#ffbb00",
+              color: "white",
+            }
+          : {}
+      }
+      onClick={onSquareClick}
+      className="square"
+    >
       {value}
     </button>
   );
